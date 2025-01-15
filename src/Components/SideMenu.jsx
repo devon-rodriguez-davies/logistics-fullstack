56a484657989609fe1b8023c7ecb14f6119b53b2
Devon Davies <devon_davies@hotmail.com>
2025-01-15 16:00:10 +0000

    Updating SideMenu to show correct page titles and started on routing

--- src/Components/SideMenu.jsx
+++ src/Components/SideMenu.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Styles/SideMenu.css';
 import { Button } from './Button';
+import Pages from '../Data/menu.json';
+import { useNavigate } from 'react-router-dom';
 
 const SideMenu = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="side-menu" style={{ padding: '10px', display: 'flex', flexDirection: 'column' }}>
-      <Button selected>Button 1</Button>
-      <Button>Button 2</Button>
-      <Button>Button 3</Button>
+    {Pages.data.map((page, index) => (
+        <Button key={index} selected={index==0} onClick={() => navigate(page.url)}>{page.title}</Button>))}
     </div>
   );
 };
